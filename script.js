f608170284aca7890679aae52d768eae5649bcfa
tialedencan <tialedencan@gmail.com>
2022-12-27 18:46:59 +0100

    Add scroll back to top button, add .webp images

---
+++ script.js
@@ -0,0 +1,27 @@
+
+//Get the button
+let mybutton = document.getElementById("back-to-top");
+let offset=100;
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > offset ||
+    document.documentElement.scrollTop > offset
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
